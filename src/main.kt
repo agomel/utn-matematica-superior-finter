24bc7bca259b9ceb46de95fe4a9890e07bafc1aa
Dylan Muszel <dylan.muszel@wolox.com.ar>
2019-10-31 23:47:15 -0300

    Altering values

--- src/main.kt
+++ src/main.kt
@@ -7,28 +7,56 @@ fun main() {
 }
 
 fun act(orderedPairs: List<OrderedPair>, option: InterpolateItem) {
+    val interpolation = option.interpolation(orderedPairs)
+    when (requestAction()) {
+
+        ShowPolynomial -> println(interpolation.polynomial)
+
+        ShowSteps -> interpolation.printSteps()
+
+        SetValueToPolynomial -> {
+            val x = requestFloat("x = ")
+            val y = interpolation.evaluate(x)
+            println()
+            println("P($x) = $y")
+            println()
+        }
+
+        AlterValues -> {
+            act(alterValues(orderedPairs), option)
+            return
+        }
+
+        Exit -> return
+    }
+
+    act(orderedPairs, option)
+}
+
+fun alterValues(orderedPairs: List<OrderedPair>): List<OrderedPair> {
+    println()
+    val mutablePairs = orderedPairs.toMutableList()
     do {
-        val interpolation = option.interpolation(orderedPairs)
-        val action = requestAction()
-        when (action) {
-
-            ShowPolynomial -> println(interpolation.polynomial)
-
-            ShowSteps -> interpolation.printSteps()
-
-            SetValueToPolynomial -> {
-                val x = requestFloat("x = ")
-                val y = interpolation.evaluate(x)
-                println()
-                println("P($x) = $y")
-                println()
-            }
-
-            AlterValues -> {
-                // TODO: Pedir valores
-                val newOrderedPair = emptyList<OrderedPair>()
-                act(newOrderedPair, option)
+        println("Elija un par ordenado para modificar:")
+        mutablePairs.forEachIndexed { i, it -> println("${i + 1}. $it") }
+        println("0. Terminar")
+        val index = request {
+            val index = it?.toIntOrNull() ?: -1
+            if (index >= 0 && index <= orderedPairs.size) {
+                index
+            } else {
+                null
             }
         }
-    } while (action != Exit)
+
+        println()
+        println("Ingrese el par ordenado:")
+        if (index > 0f) {
+            val x = requestFloat("x = ")
+            val y = requestFloat("y = ")
+            mutablePairs[index - 1] = x to y
+        }
+        println()
+    } while (index > 0f)
+    return mutablePairs
 }
