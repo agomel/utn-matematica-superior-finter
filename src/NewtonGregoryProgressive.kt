cf27713dec11d01fba390d5e14ebc6c21820efa5
Florencia Lipchak <florencia.lipchak@despegar.com>
2019-11-05 14:38:29 -0300

    Evaluar en un punto

--- src/NewtonGregoryProgressive.kt
+++ src/NewtonGregoryProgressive.kt
@@ -4,24 +4,38 @@ class NewtonGregoryProgressive(private val points: List<OrderedPair>) : Interpol
 
     override val polynomial: String
         get() {
-            return if (pointsAreEquispaced())
-                ""
+            return if (pointsAreEquispaced)
+                "P(X) = ${points.first().second}${deltas.foldIndexed(""){index, str, list -> "$str ${list.first().let { if (!it.equals(0F)) "+ ${calculateEquispacedTerm(index, it)}${stringXValues(index)}" else "" }}"}}"
             else
-                "P(X) = ${points.first().second} ${deltas.foldIndexed(""){index, str, list -> "$str ${list.first().let { if (it > 0) "+ $it${stringXValues(index)}" else "" }}"}}"
+                "P(X) = ${points.first().second}${deltas.foldIndexed(""){index, str, list -> "$str ${list.first().let { if (!it.equals(0F)) "+ $it${stringXValues(index)}" else "" }}"}}"
         }
 
     override fun printSteps() {
+        printSeparator()
+
         println("1. Calculamos la tabla de diferencias finitas utilizando los puntos dados:")
         differencesTableString()
+
+        if(pointsAreEquispaced) {
+            println()
+            println("2. Calculamos cada término del polinomio")
+            println("P(X) = ${points.first().second} ${deltas.foldIndexed(""){index, str, list -> "$str ${list.first().let { if (!it.equals(0)) "+ ${stringTerm(index, it)}${stringXValues(index)}" else "" }}"}}")
+            println()
+            println("Los puntos son equiespaciados")
+        } else {
+            println()
+            println("Los puntos no son equiespaciados")
+        }
+
+        printSeparator()
     }
 
-    override fun evaluate(k: Float): Float {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun evaluate(k: Float): Float =
+        points.first().second + (deltas.foldIndexed(0F){index, acc, list -> acc + term(index, list.first()) * xValues(index, k) })
 
     private val deltas: List<List<Float>> by lazy {
         points.map { it.second }.let {
-            if (pointsAreEquispaced())
+            if (pointsAreEquispaced)
                 calculateDeltasEquispaced(it)
             else
                 calculateDeltasNotEquispaced(it)
@@ -47,22 +61,31 @@ class NewtonGregoryProgressive(private val points: List<OrderedPair>) : Interpol
                 if (it.isEmpty()) accum else calculateDeltasEquispaced(it, accum.plusElement(it))
             }
 
-    private fun pointsAreEquispaced(): Boolean {
-        if(points.size > 1) return false
-        return points.map { it.first }.zipWithNext { a, b -> b - a == h }.all { it } //chequeo que todos los puntos cumplan esa distancia
+    private val pointsAreEquispaced: Boolean by lazy{
+        if(points.size < 2) false
+        else points.map { it.first }.zipWithNext { a, b -> b - a == h }.all { it } //chequeo que todos los puntos cumplan esa distancia
     }
 
     private fun stringXValues(i: Int) =
         points.filterIndexed{ index, _ -> index <= i }.fold(""){str, value -> "$str(x - ${value.first})"}
 
+    private fun xValues(i: Int, x: Float) =
+        points.filterIndexed{ index, _ -> index <= i }.fold(1F){accum, value -> accum * (x - value.first)}
+
     private fun differencesTableString() {
-        println("|  X  |  Y  | ${stringTitles(deltas)}")
-        points.forEachIndexed { index, (x, y) -> println("| $x | $y |${stringDeltas(deltas, index)}") }
+        println("|   X   |   Y   | ${stringTitles(deltas)}")
+        points.forEachIndexed { index, (x, y) -> println("|  $x  |  $y  |${stringDeltas(deltas, index)}") }
 
     }
 
-    private fun calculateTerm(index: Int, delta: Float): Float = //pow es elevar
-        delta / (factorial(index) * h.pow(index))
+    private fun term(index: Int, delta: Float) =
+        if (!pointsAreEquispaced) delta else calculateEquispacedTerm(index, delta)
+
+    private fun calculateEquispacedTerm(index: Int, delta: Float): Float = //pow es elevar
+        delta / (factorial(index + 1) * h.pow(index + 1))
+
+    private fun stringTerm(index: Int, delta: Float): String =
+        "[$delta / ${index.let { if (it == 0) h.toString() else "(${it + 1}! * $h^${it + 1})"}}]"
 
     private fun factorial(num: Int): Long {
         var factorial: Long = 1
@@ -72,8 +95,8 @@ class NewtonGregoryProgressive(private val points: List<OrderedPair>) : Interpol
     }
 
     private fun stringTitles(deltas: List<List<Float>>) =
-            deltas.foldIndexed("") { index, acc, _ -> "$acc Δ${index.let { if (it > 0) "^${it+1}" else "" }}  |" }
+        deltas.foldIndexed("") { index, acc, _ -> "$acc  Δ${index.let { if (it > 0) "^${it+1}" else "" }}   |" }
 
     private fun stringDeltas(deltas: List<List<Float>>, index: Int): String =
-        deltas.fold("") { acc, list -> if (list.size > index) acc + " ${list[index]} |" else acc }
+        deltas.fold("") { acc, list -> if (list.size > index) "$acc ${list[index]}  |" else acc }
 }
